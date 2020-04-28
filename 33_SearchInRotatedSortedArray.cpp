aa04df5c93f28ecaf0a5f1dc243064f6827438e7
Rohit Agarwal <60344453+rohit9798@users.noreply.github.com>
2020-04-28 20:30:39 +0530

    Create 33_SearchInRotatedSortedArray.cpp

---
+++ 33_SearchInRotatedSortedArray.cpp
@@ -0,0 +1,54 @@
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+
+
+class Solution {
+public:
+
+    int search(vector<int>& nums, int key) {
+        
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int mid;
+        while(low < high)
+        {
+            mid = (low + high)/2;
+            if(nums[mid] > nums[high])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        int k = low;
+        low = 0;
+        high = n - 1;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            int realmid = (mid + k) % n;
+            if(nums[realmid] == key)
+                return realmid;
+            else if(nums[realmid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+};
