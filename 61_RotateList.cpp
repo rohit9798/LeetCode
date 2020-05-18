8400ef7e0a8442758363be465a837c2399424f44
Rohit Agarwal <60344453+rohit9798@users.noreply.github.com>
2020-05-18 12:21:13 +0530

    Create 61_RotateList.cpp

---
+++ 61_RotateList.cpp
@@ -0,0 +1,54 @@
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        if(head == NULL || head -> next == NULL)
+            return head;
+        ListNode *ptr = head, *qtr = head;
+        int len = 1;
+        while(ptr -> next!= NULL)
+        {
+            ptr = ptr -> next;
+            len++;
+        }
+        ptr -> next = head;
+        k = k % len;
+        for(int i = 0; i < len - k; i++)
+            ptr = ptr -> next;
+        qtr = ptr -> next;
+        ptr -> next = NULL;
+        cout<<len<<k;
+        return qtr;
+    }
+};
