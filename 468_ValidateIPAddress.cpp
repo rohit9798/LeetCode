abc9e16423d674a5d86c99c1e6cf525c8e19a854
Rohit Agarwal <60344453+rohit9798@users.noreply.github.com>
2020-06-16 22:12:36 +0530

    Create 468_ValidateIPAddress.cpp

---
+++ 468_ValidateIPAddress.cpp
@@ -0,0 +1,110 @@
+/*
+Write a function to check whether an input string is a valid IPv4 address or IPv6 address or neither.
+IPv4 addresses are canonically represented in dot-decimal notation, which consists of four decimal numbers, each ranging from 0 to 255, separated by dots ("."), e.g.,172.16.254.1;
+Besides, leading zeros in the IPv4 is invalid. For example, the address 172.16.254.01 is invalid.
+IPv6 addresses are represented as eight groups of four hexadecimal digits, each group representing 16 bits. The groups are separated by colons (":"). For example, the address 2001:0db8:85a3:0000:0000:8a2e:0370:7334 is a valid one. Also, we could omit some leading zeros among four hexadecimal digits and some low-case characters in the address to upper-case ones, so 2001:db8:85a3:0:0:8A2E:0370:7334 is also a valid IPv6 address(Omit leading zeros and using upper cases).
+However, we don't replace a consecutive group of zero value with a single empty group using two consecutive colons (::) to pursue simplicity. For example, 2001:0db8:85a3::8A2E:0370:7334 is an invalid IPv6 address.
+Besides, extra leading zeros in the IPv6 is also invalid. For example, the address 02001:0db8:85a3:0000:0000:8a2e:0370:7334 is invalid.
+Note: You may assume there is no extra space or special characters in the input string.
+Example 1:
+Input: "172.16.254.1"
+Output: "IPv4"
+Explanation: This is a valid IPv4 address, return "IPv4".
+Example 2:
+Input: "2001:0db8:85a3:0:0:8A2E:0370:7334"
+Output: "IPv6"
+Explanation: This is a valid IPv6 address, return "IPv6".
+Example 3:
+Input: "256.256.256.256"
+Output: "Neither"
+Explanation: This is neither a IPv4 address nor a IPv6 address.
+*/
+class Solution {
+public:
+    string validIPAddress(string IP) {
+        
+        int i, size = IP.size();
+        vector<string>v;
+        string str = "", str1;
+        char temp, temp1;
+        string validIPv6Chars = "0123456789abcdefABCDEF";
+        string validIPv4Chars = "0123456789";
+        for( i = 0; i < size; i++ )
+        {
+            if( IP[i] == '.' )
+            {
+                temp = '.';
+                temp1 = ':';
+                break;
+            }
+            
+            if( IP[i] == ':' )
+            {
+                temp = ':';
+                temp1 = '.';
+                break;
+            }
+            
+        }
+        for( i = 0; i < size; i++ )
+        {
+            if( IP[i] == temp1 )
+                return "Neither";
+            if( IP[i] == temp )
+            {
+                if( str == "" )
+                    return "Neither";
+                if( str.size() > 4 )
+                    return "Neither";
+                v.push_back(str);
+                str = "";
+            }
+            else
+            {
+                str += IP[i];
+                if( temp == ':' )
+                {
+                    if (validIPv6Chars.find(IP[i]) == string::npos)
+    	    	        return "Neither";
+                }
+                else if( temp == '.' )
+                {
+                    if (validIPv4Chars.find(IP[i]) == string::npos)
+    	    	        return "Neither";
+                }
+            }
+        }
+        if( str != "" )
+            v.push_back(str);
+        if( str.size() > 4 )
+            return "Neither";
+        if( str == "" )
+            return "Neither";
+        if( v.size() == 4 )
+        {
+            for( i = 0; i < 4; i++ )
+            {
+                if( v[i][0] == '0' && v[i].size() > 1 )
+                    return "Neither";
+                int val = stoi(v[i]);
+                if( 0 <= val && val <= 255 )
+                {
+                    continue;
+                }
+                return "Neither";
+            }
+            return "IPv4";
+        }
+        
+        if( v.size() == 8 )
+        {
+            for( i = 0; i < 8; i++ )
+            {
+                if( v[i].size() > 4 )
+                    return "Neither";
+            }
+            return "IPv6";
+        }
+        return "Neither";
+    }
+};
