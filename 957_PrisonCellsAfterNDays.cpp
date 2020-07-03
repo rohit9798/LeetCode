64b77d23590fa2ded5af445d5a57312ec8f18ee1
Rohit Agarwal <60344453+rohit9798@users.noreply.github.com>
2020-07-03 22:13:57 +0530

    Create 957_PrisonCellsAfterNDays.cpp

---
+++ 957_PrisonCellsAfterNDays.cpp
@@ -0,0 +1,61 @@
+There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+Otherwise, it becomes vacant.
+(Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+ 
+
+Example 1:
+
+Input: cells = [0,1,0,1,1,0,0,1], N = 7
+Output: [0,0,1,1,0,0,0,0]
+Explanation: 
+The following table summarizes the state of the prison on each day:
+Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+
+Example 2:
+
+Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+Output: [0,0,1,1,1,1,1,0]
+
+
+
+
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        
+        int j = 0;
+        vector<int> result(cells.size());
+        for(int i = 0; i < ((N - 1) % 14) + 1; i++)
+        {
+            result[j] = 0;
+            for(j = 1; j < 7; j++)
+            {
+                if(cells[j - 1] == cells[j + 1])
+                {
+                    result[j] = 1;
+                }
+                else
+                    result[j] = 0;
+            }
+            result[j] = 0;
+            cells = result;
+        }
+        return result;
+    }
+};
