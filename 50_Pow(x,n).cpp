c1515418e677ea5907c2aa5446f974308889dfb1
Rohit Agarwal <60344453+rohit9798@users.noreply.github.com>
2020-07-08 12:09:06 +0530

    Create 50_Pow(x,n).cpp

---
+++ 50_Pow(x,n).cpp
@@ -0,0 +1,46 @@
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n == 0)
+            return 1;
+        if(n > 0)
+        {
+            if(n == 1)
+                return x;
+            double temp = myPow(x, n / 2);
+            if(n % 2 == 0)
+                return  temp * temp;
+            else
+                return x * temp * temp;
+        }
+        else
+        {
+            if(n == -1)
+                return 1 / x;
+            double temp = myPow(x, n / 2);
+            if(n % 2 == 0)
+                return temp * temp;
+            else
+                return (1 / x) * temp * temp;
+        }
+    }
+};
