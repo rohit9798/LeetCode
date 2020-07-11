e01becc7e6fb97aa742303109dcdf5a0439f88be
Rohit Agarwal <60344453+rohit9798@users.noreply.github.com>
2020-07-11 14:03:10 +0530

    Create 1309_DecryptStringFromAlphabetToIntegerMapping.cpp

---
+++ 1309_DecryptStringFromAlphabetToIntegerMapping.cpp
@@ -0,0 +1,53 @@
+Given a string s formed by digits ('0' - '9') and '#' . We want to map s to English lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively. 
+Return the string formed after mapping.
+
+It's guaranteed that a unique mapping will always exist.
+
+ 
+
+Example 1:
+
+Input: s = "10#11#12"
+Output: "jkab"
+Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+Example 2:
+
+Input: s = "1326#"
+Output: "acz"
+Example 3:
+
+Input: s = "25#"
+Output: "y"
+Example 4:
+
+Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+Output: "abcdefghijklmnopqrstuvwxyz"
+
+
+
+class Solution {
+public:
+    string freqAlphabets(string s) {
+        
+        string result = "";
+        string temp = "";
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(i + 2 < s.length() && s[i + 2] == '#')
+            {
+                int number = (s[i] - '0') * 10 + (s[i + 1] -'0');
+                result += 'a' + (number - 1);
+                i += 2;
+            }
+            else
+            {
+                int number = s[i] - '0';
+                result += 'a' + (number - 1);
+            }
+        }
+        return result;
+    }
+};
